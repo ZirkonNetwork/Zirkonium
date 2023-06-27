b720535e3c7b0a6b417cb9f3f9dde337ad27547e
Elephant1214 <nathangetsmail@gmail.com>
2023-06-27 14:26:38 -0600

    Changed config package name and updated deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import java.nio.charset.StandardCharsets
 
 plugins {
     java
-    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+    id("io.papermc.paperweight.patcher") version "1.5.5"
 }
 
 repositories {
@@ -17,7 +17,7 @@ repositories {
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
     decompiler("net.minecraftforge:forgeflower:2.0.627.2")
-    paperclip("io.papermc:paperclip:3.0.2")
+    paperclip("io.papermc:paperclip:3.0.3")
 }
 
 subprojects {

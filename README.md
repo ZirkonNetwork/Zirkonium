aee0afea11d0fb59bafcb62e2c66e6f107a64052
Elephant1214 <nathangetsmail@gmail.com>
2024-01-02 10:08:25 -0700

    Initial 1.20.4

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 Purpur fork originally for Zirkon Network
 ## Features
 - [No-Chat-Reports Mod](https://github.com/Aizistral-Studios/No-Chat-Reports)
-- [Smooth Boot Mod](https://github.com/UltimateBoomer/mc-smoothboot)
 - [Dynamic View](https://github.com/ldtteam/DynView)
 - Higher maximum enchantment levels modified with multiplication by the set value
 - Modifiable maximum anvil levels without an extra plugin (can be overridden by plugins)
+#### Old Features
+These features have been removed for various reasons but are still available under `/patches/removed`.
+If you for some reason still want to use them, they must be re-implemented.
+You will receive no support for the features or for re-implementing them.
+- [Smooth Boot Mod](https://github.com/UltimateBoomer/mc-smoothboot)
+  - Issues solved by SB were fixed in 1.19.4
 ## Building
 Run `./gradlew applyPatches`, `./gradlew build`, and then `./gradlew createReobfPaperclipJar` to get a Paperclip jar to use for a server
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,75 +1,85 @@
-import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
-import java.nio.charset.StandardCharsets
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java
+    `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.7-SNAPSHOT"
+    id("io.papermc.paperweight.patcher") version "1.5.11"
 }
 
-repositories {
-    mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
-        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(17)
+        }
     }
 }
 
-dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
-    decompiler("net.minecraftforge:forgeflower:2.0.627.2")
-    paperclip("io.papermc:paperclip:3.0.3")
-}
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
 subprojects {
-    apply(plugin = "java")
-    
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(17))
+    tasks.withType<JavaCompile>().configureEach {
+        options.encoding = Charsets.UTF_8.name()
+        options.release = 17
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+    tasks.withType<Test> {
+        testLogging {
+            showStackTraces = true
+            exceptionFormat = TestExceptionFormat.FULL
+            events(TestLogEvent.STANDARD_OUT)
         }
     }
 
-    tasks.withType<Javadoc>().all {
-        enabled = false // Javadoc kept throwing errors and failing to build, didn't need it anyway
-    }
-
-    tasks.withType<JavaCompile>().configureEach {
-        options.encoding = StandardCharsets.UTF_8.name()
-        options.release.set(17)
-    }
-
     repositories {
-        mavenLocal()
         mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven(paperMavenPublicUrl)
         maven("https://jitpack.io")
     }
 }
 
-paperweight {
-    serverProject.set(project(":Zirkonium-Server"))
+repositories {
+    mavenCentral()
+    maven(paperMavenPublicUrl) {
+        content {
+            onlyForConfigurations(configurations.paperclip.name)
+        }
+    }
+}
 
-    remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.8.10:fat")
+    decompiler("net.minecraftforge:forgeflower:2.0.627.2")
+    paperclip("io.papermc:paperclip:3.0.3")
+}
+
+paperweight {
+    serverProject = project(":Zirkonium-Server")
+
+    remapRepo = paperMavenPublicUrl
+    decompileRepo = paperMavenPublicUrl
 
     useStandardUpstream("purpur") {
-        url.set(github("PurpurMC", "Purpur"))
-        ref.set(providers.gradleProperty("purpurRef"))
+        url = github("PurpurMC", "Purpur")
+        ref = providers.gradleProperty("purpurCommit")
 
         withStandardPatcher {
-            apiSourceDirPath.set("Purpur-API")
-            serverSourceDirPath.set("Purpur-Server")
+            baseName("Purpur")
 
-            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
-            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+            apiPatchDir = layout.projectDirectory.dir("patches/api")
+            apiOutputDir = layout.projectDirectory.dir("Zirkonium-API")
 
-            apiOutputDir.set(layout.projectDirectory.dir("Zirkonium-API"))
-            serverOutputDir.set(layout.projectDirectory.dir("Zirkonium-Server"))
+            serverPatchDir = layout.projectDirectory.dir("patches/server")
+            serverOutputDir = layout.projectDirectory.dir("Zirkonium-Server")
         }
     }
 }
